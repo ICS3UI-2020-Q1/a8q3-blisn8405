9a6392d1ff9450a2ec865366d2c8de38c0d2ef87
blisn8405 <blisn8405@wrdsb.ca>
2020-10-30 17:20:40 +0000

    q3

--- Main.java
+++ Main.java
@@ -5,6 +5,18 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
+   JPanel mainPanel;
+
+  JLabel firstLabel;
+  JLabel secondLabel;
+
+  JTextField firstInput;
+  JTextField secondInput;
+
+  JButton ftoCButton;
+  JButton ctoFButton;
+
+  JTextArea outputArea;
   
 
 
@@ -19,7 +31,71 @@ public class Main implements Runnable, ActionListener{
     // shows the window
     frame.setVisible(true);
  
+     //initialize the main JPanel
+    mainPanel = new JPanel();
+    //disable any layout helpers
+    mainPanel.setLayout(null);
+
+    //create the side labels
+    firstLabel = new JLabel("Degrees Fahrenheit");
+    secondLabel = new JLabel("Degrees Celsius");
+
+     //layout the labels by setting the coordinate and size
+    //setBounds(x,y,width,height)
+    firstLabel.setBounds(50,50,100,30);
+    secondLabel.setBounds(50,90,100,30);
+
+    //initialize the input text fields
+    firstInput = new JTextField();
+    secondInput = new JTextField();
+
+    //set the location and size
+    firstInput.setBounds(150,50,75,30);
+    secondInput.setBounds(150,90,75,30);
+
+     //initialize the buttons
+    ftoCButton = new JButton("F-> C");
+    ctoFButton = new JButton("C-> F");
     
+     //set the size and location of the buttons
+    ftoCButton.setBounds(220,50,70,30);
+    ctoFButton.setBounds(220,90,70,30);
+    
+  
+    //add an action listener to the buttons
+    ftoCButton.addActionListener(this);
+    ctoFButton.addActionListener(this);
+   
+
+    //set the action command on the button
+    ftoCButton.setActionCommand("F-> C");
+    ctoFButton.setActionCommand("C-> F");
+  
+   //initialize the text Areas
+   outputArea = new JTextArea();
+
+   // add to main panel
+   mainPanel.add(outputArea);
+
+
+   //set size and locaiton of text Areas
+   outputArea.setBounds(150,50,75,30);
+
+    //add the buttons to the panel
+    mainPanel.add(ftoCButton);
+    mainPanel.add(ctoFButton);
+  
+
+    //add the text fields to the panel
+    mainPanel.add(firstInput);
+    mainPanel.add(secondInput);
+    
+    //add the labels to the main pane;
+    mainPanel.add(firstLabel);
+    mainPanel.add(secondLabel);
+
+    // add the panel to the window
+    frame.add(mainPanel);
 
   }
 
@@ -27,9 +103,17 @@ public class Main implements Runnable, ActionListener{
   public void actionPerformed(ActionEvent e){
     // get the command from the action
     String command = e.getActionCommand();
+   // get the text from each text boxes
+    String firstLabel = firstInput.getText();
+     //change the String into an integer
+    int Celsius = Integer.parseInt(firstLabel);
 
+ int Fahrenheit = ((Celsius*9)/5 +32);
+// output the answer to the user
+    if(command.equals("C-> F")){
+      outputArea.setText("" + Fahrenheit);
   }
-
+  }
   // Main method to start our program
   public static void main(String[] args){
     // Creates an instance of our program
